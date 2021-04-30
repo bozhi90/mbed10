524b3ba7321a803862395f9e1d12f310589cd026
bozhi Tsai <bozhi9004@gmaill.com>
2021-04-29 17:44:53 -0700

    add main.cpp

---
+++ 10_2_MQTT_Python/BUILD/B_L4S5I_IOT01A/GCC_ARM/mbed-os/connectivity/libraries/nanostack-libservice/source/nsdynmemtracker/nsdynmem_tracker_lib.d
@@ -0,0 +1,9 @@
+BUILD/B_L4S5I_IOT01A/GCC_ARM/mbed-os/connectivity/libraries/nanostack-libservice/source/nsdynmemtracker/nsdynmem_tracker_lib.o: \
+ /home/ee2405/ee2405/mbed-os/connectivity/libraries/nanostack-libservice/source/nsdynmemtracker/nsdynmem_tracker_lib.c \
+ BUILD/B_L4S5I_IOT01A/GCC_ARM/mbed_config.h \
+ /home/ee2405/ee2405/mbed-os/connectivity/libraries/nanostack-libservice/mbed-client-libservice/ns_types.h \
+ /home/ee2405/ee2405/mbed-os/connectivity/libraries/nanostack-libservice/mbed-client-libservice/nsdynmem_tracker_lib.h \
+ /home/ee2405/ee2405/mbed-os/connectivity/libraries/nanostack-libservice/mbed-client-libservice/platform/arm_hal_interrupt.h \
+ /home/ee2405/ee2405/mbed-os/connectivity/libraries/nanostack-libservice/mbed-client-libservice/nsdynmemLIB.h \
+ /home/ee2405/ee2405/mbed-os/connectivity/libraries/nanostack-libservice/mbed-client-libservice/ns_types.h \
+ /home/ee2405/ee2405/mbed-os/connectivity/libraries/nanostack-libservice/mbed-client-libservice/nsdynmem_tracker.h
